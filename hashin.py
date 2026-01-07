28ed9411cf8933d3feca7af737a0d916aec301cc
Peash <107058247+Peash02@users.noreply.github.com>
2026-01-07 10:23:25 +0530

    Delete hashin.py

--- hashin.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def get_hash(txt):
-    sha_signature = hashlib.sha256(txt.encode()).hexdigest()
-    return sha_signature
-
-txt = input()
-print(f"Text:{txt}")
-print(f"SHA-256 Hash: {get_hash(txt)}")
